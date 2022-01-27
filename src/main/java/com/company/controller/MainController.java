8d389037ad5206d5150cdf9d7fb3917b91294e86
Павел <Hegativenn@yandex.ru>
2022-01-27 22:38:17 +0300

    added main page for uploading files and controller and result page

---
+++ src/main/java/com/company/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.company.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String MainForm() {
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String uploadFile(@RequestParam("file1") MultipartFile file1,
+                             @RequestParam("file2") MultipartFile file2){
+
+
+
+        return "result";
+    }
+
+}
